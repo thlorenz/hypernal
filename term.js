03eb5be84669bde2e8d45007cf3bc2aa54f7d14b
Thorsten Lorenz <thlorenz@gmx.de>
2013-04-28 16:52:55 -0400

    scroll and scrollDisp

--- term.js
+++ term.js
@@ -111,58 +111,8 @@ require('./lib/focus')(Terminal);
 require('./lib/open')(Terminal);
 require('./lib/destroy')(Terminal);
 require('./lib/refresh')(Terminal);
-
-Terminal.prototype.scroll = function() {
-    var row;
-
-    if (++this.ybase === Terminal.scrollback) {
-        this.ybase = this.ybase / 2 | 0;
-        this.lines = this.lines.slice(-(this.ybase + this.rows) + 1);
-    }
-
-    this.ydisp = this.ybase;
-
-    // last line
-    row = this.ybase + this.rows - 1;
-
-    // subtract the bottom scroll region
-    row -= this.rows - 1 - this.scrollBottom;
-
-    if (row === this.lines.length) {
-        // potential optimization:
-        // pushing is faster than splicing
-        // when they amount to the same
-        // behavior.
-        this.lines.push(this.blankLine());
-    } else {
-        // add our new line
-        this.lines.splice(row, 0, this.blankLine());
-    }
-
-    if (this.scrollTop !== 0) {
-        if (this.ybase !== 0) {
-            this.ybase--;
-            this.ydisp = this.ybase;
-        }
-        this.lines.splice(this.ybase + this.scrollTop, 1);
-    }
-
-    // this.maxRange();
-    this.updateRange(this.scrollTop);
-    this.updateRange(this.scrollBottom);
-};
-
-Terminal.prototype.scrollDisp = function(disp) {
-    this.ydisp += disp;
-
-    if (this.ydisp > this.ybase) {
-        this.ydisp = this.ybase;
-    } else if (this.ydisp < 0) {
-        this.ydisp = 0;
-    }
-
-    this.refresh(0, this.rows - 1);
-};
+require('./lib/scroll')(Terminal);
+require('./lib/scrollDisp')(Terminal);
 
 require('./lib/write')(Terminal);
 
