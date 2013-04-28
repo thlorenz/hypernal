dc8cc1b4fb887880df2a638b37afe1ce3e39a784
Thorsten Lorenz <thlorenz@gmx.de>
2013-04-28 16:17:27 -0400

    focus

--- term.js
+++ term.js
@@ -98,25 +98,8 @@ inherits(Terminal, EventEmitter);
 
 require('./lib/colors')(Terminal);
 require('./lib/options')(Terminal);
+require('./lib/focus')(Terminal);
 
-
-/**
-* Focused Terminal
-*/
-
-Terminal.focus = null;
-
-Terminal.prototype.focus = function() {
-    if (Terminal.focus === this) return;
-    if (Terminal.focus) {
-        Terminal.focus.cursorState = 0;
-        Terminal.focus.refresh(Terminal.focus.y, Terminal.focus.y);
-        if (Terminal.focus.sendFocus) Terminal.focus.send('\x1b[O');
-    }
-    Terminal.focus = this;
-    if (this.sendFocus) this.send('\x1b[I');
-    this.showCursor();
-};
 
 /**
 * Global Events for key handling
