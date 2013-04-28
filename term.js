a7af07098c0ee6bc26a1ee7dafec8d9051884199
Thorsten Lorenz <thlorenz@gmx.de>
2013-04-28 16:49:17 -0400

    cursor

--- term.js
+++ term.js
@@ -111,38 +111,9 @@ require('./lib/focus')(Terminal);
 require('./lib/open')(Terminal);
 require('./lib/destroy')(Terminal);
 require('./lib/refresh')(Terminal);
+require('./lib/cursor')(Terminal);
 
 
-Terminal.prototype.cursorBlink = function() {
-    if (Terminal.focus !== this) return;
-    this.cursorState ^= 1;
-    this.refresh(this.y, this.y);
-};
-
-Terminal.prototype.showCursor = function() {
-    if (!this.cursorState) {
-        this.cursorState = 1;
-        this.refresh(this.y, this.y);
-    } else {
-        // Temporarily disabled:
-        // this.refreshBlink();
-    }
-};
-
-Terminal.prototype.startBlink = function() {
-    if (!Terminal.cursorBlink) return;
-    var self = this;
-    this._blinker = function() {
-        self.cursorBlink();
-    };
-    this._blink = setInterval(this._blinker, 500);
-};
-
-Terminal.prototype.refreshBlink = function() {
-    if (!Terminal.cursorBlink) return;
-    clearInterval(this._blink);
-    this._blink = setInterval(this._blinker, 500);
-};
 
 Terminal.prototype.scroll = function() {
     var row;
