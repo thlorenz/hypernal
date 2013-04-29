f41c5e077204762ecfa0a93de596adfba94815eb
Thorsten Lorenz <thlorenz@gmx.de>
2013-04-28 21:26:27 -0400

    disabling keypress

--- term.js
+++ term.js
@@ -120,33 +120,6 @@ require('./lib/write')(Terminal);
 require('./lib/setgLevel');
 require('./lib/setgCharset');
 
-Terminal.prototype.keyPress = function(ev) {
-    var key;
-
-    cancel(ev);
-
-    if (ev.charCode) {
-        key = ev.charCode;
-    } else if (ev.which == null) {
-        key = ev.keyCode;
-    } else if (ev.which !== 0 && ev.charCode !== 0) {
-        key = ev.which;
-    } else {
-        return false;
-    }
-
-    if (!key || ev.ctrlKey || ev.altKey || ev.metaKey) return false;
-
-    key = String.fromCharCode(key);
-
-    this.emit('keypress', key, ev);
-    this.emit('key', key, ev);
-
-    this.showCursor();
-    this.handler(key);
-
-    return false;
-};
 
 Terminal.prototype.send = function(data) {
     var self = this;
