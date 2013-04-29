aeb69257d4148d928b52e230395d9c3a4399f9e6
Thorsten Lorenz <thlorenz@gmx.de>
2013-04-29 07:45:20 -0400

    csi columns -- not enabled

--- term.js
+++ term.js
@@ -175,51 +175,6 @@ require('./lib/csi/repeatPrecedingCharacter')(Terminal);
 require('./lib/csi/tabClear')(Terminal);
 require('./lib/csi/softReset')(Terminal);
 
-
-
-
-
-
-// CSI P m SP }
-// Insert P s Column(s) (default = 1) (DECIC), VT420 and up.
-// NOTE: xterm doesn't enable this code by default.
-Terminal.prototype.insertColumns = function() {
-    var param = params[0],
-        l = this.ybase + this.rows,
-        ch = [this.curAttr, ' '] // xterm?
-        ,
-        i;
-
-    while (param--) {
-        for (i = this.ybase; i < l; i++) {
-            this.lines[i].splice(this.x + 1, 0, ch);
-            this.lines[i].pop();
-        }
-    }
-
-    this.maxRange();
-};
-
-// CSI P m SP ~
-// Delete P s Column(s) (default = 1) (DECDC), VT420 and up
-// NOTE: xterm doesn't enable this code by default.
-Terminal.prototype.deleteColumns = function() {
-    var param = params[0],
-        l = this.ybase + this.rows,
-        ch = [this.curAttr, ' '] // xterm?
-        ,
-        i;
-
-    while (param--) {
-        for (i = this.ybase; i < l; i++) {
-            this.lines[i].splice(this.x, 1);
-            this.lines[i].push(ch);
-        }
-    }
-
-    this.maxRange();
-};
-
 /**
 * Character Sets
 */
