4a21f7c78992d897c3a6471a7386c12577c06905
Thorsten Lorenz <thlorenz@gmx.de>
2013-04-29 07:20:52 -0400

    more into csi cursor and csi scroll

--- term.js
+++ term.js
@@ -169,57 +169,15 @@ require('./lib/csi/insert-delete')(Terminal);
 require('./lib/csi/position')(Terminal);
 require('./lib/csi/sendDeviceAttributes')(Terminal);
 require('./lib/csi/cursor')(Terminal);
+require('./lib/csi/scroll')(Terminal);
 
 
-// CSI Ps ; Ps r
-// Set Scrolling Region [top;bottom] (default = full size of win-
-// dow) (DECSTBM).
-// CSI ? Pm r
-Terminal.prototype.setScrollRegion = function(params) {
-    if (this.prefix) return;
-    this.scrollTop = (params[0] || 1) - 1;
-    this.scrollBottom = (params[1] || this.rows) - 1;
-    this.x = 0;
-    this.y = 0;
-};
 
 
 /**
 * Lesser Used
 */
 
-// CSI Ps I
-// Cursor Forward Tabulation Ps tab stops (default = 1) (CHT).
-Terminal.prototype.cursorForwardTab = function(params) {
-    var param = params[0] || 1;
-    while (param--) {
-        this.x = this.nextStop();
-    }
-};
-
-// CSI Ps S Scroll up Ps lines (default = 1) (SU).
-Terminal.prototype.scrollUp = function(params) {
-    var param = params[0] || 1;
-    while (param--) {
-        this.lines.splice(this.ybase + this.scrollTop, 1);
-        this.lines.splice(this.ybase + this.scrollBottom, 0, this.blankLine());
-    }
-    // this.maxRange();
-    this.updateRange(this.scrollTop);
-    this.updateRange(this.scrollBottom);
-};
-
-// CSI Ps T Scroll down Ps lines (default = 1) (SD).
-Terminal.prototype.scrollDown = function(params) {
-    var param = params[0] || 1;
-    while (param--) {
-        this.lines.splice(this.ybase + this.scrollBottom, 1);
-        this.lines.splice(this.ybase + this.scrollTop, 0, this.blankLine());
-    }
-    // this.maxRange();
-    this.updateRange(this.scrollTop);
-    this.updateRange(this.scrollBottom);
-};
 
 // CSI Ps ; Ps ; Ps ; Ps ; Ps T
 // Initiate highlight mouse tracking. Parameters are
