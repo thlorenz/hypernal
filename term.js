fb0e2fd6eb9d5e3d2ae5da18010dc4bccd241adf
Thorsten Lorenz <thlorenz@gmx.de>
2013-04-29 07:42:46 -0400

    csi softReset

--- term.js
+++ term.js
@@ -173,27 +173,12 @@ require('./lib/csi/scroll')(Terminal);
 require('./lib/csi/rectangle')(Terminal);
 require('./lib/csi/repeatPrecedingCharacter')(Terminal);
 require('./lib/csi/tabClear')(Terminal);
+require('./lib/csi/softReset')(Terminal);
 
 
 
 
 
-// CSI ! p Soft terminal reset (DECSTR).
-// http://vt100.net/docs/vt220-rm/table4-10.html
-Terminal.prototype.softReset = function(params) {
-    this.cursorHidden = false;
-    this.insertMode = false;
-    this.originMode = false;
-    this.wraparoundMode = false; // autowrap
-    this.applicationKeypad = false; // ?
-    this.scrollTop = 0;
-    this.scrollBottom = this.rows - 1;
-    this.curAttr = this.defAttr;
-    this.x = this.y = 0; // ?
-    this.charset = null;
-    this.glevel = 0; // ??
-    this.charsets = [null]; // ??
-};
 
 // CSI P m SP }
 // Insert P s Column(s) (default = 1) (DECIC), VT420 and up.
